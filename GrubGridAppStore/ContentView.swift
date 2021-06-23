d1b45b2dd891e783d29f6108f536b2abc5b93e78
Will McIntosh <william.michael.mcintosh@gmail.com>
2021-06-23 16:03:25 -0700

    Initial Commit

---
+++ GrubGridAppStore/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GrubGridAppStore
+//
+//  Created by Will McIntosh on 6/23/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GrubGridAppStore/GrubGridAppStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrubGridAppStoreApp.swift
+//  GrubGridAppStore
+//
+//  Created by Will McIntosh on 6/23/21.
+//
+
+import SwiftUI
+
+@main
+struct GrubGridAppStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
